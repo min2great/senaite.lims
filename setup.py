32515d88b9139f4ad96f96432d696dd85f6725d5
valeriozhang <valeriozhang14@gmail.com>
2020-12-11 19:12:36 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         "valer.core.spotlight>=1.0.3",
         "valer.impress>=1.2.4",
         "valer.jsonapi>=1.2.3",
+        "valer.panic = 1.0.0,
         "setuptools",
     ],
     extras_require={
