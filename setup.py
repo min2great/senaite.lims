856859d69393bcc54c0956e613968b1cfa943ba8
valeriozhang <valeriozhang14@gmail.com>
2020-12-15 00:42:38 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-version = "1.3.4rc3"
+version = "1.3.4rc4"
 
 with open("docs/About.rst", "r") as fh:
     long_description = fh.read()
@@ -40,12 +40,12 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        "valer.core==1.3.5rc3",
-        "valer.core.listing==1.5.3rc3",
-        "valer.core.spotlight==1.0.3rc3",
-        "valer.impress==1.2.4rc3",
-        "valer.jsonapi==1.2.3rc3",
-        "valer.panic==1.0.1rc3",
+        "valer.core==1.3.5rc4",
+        "valer.core.listing==1.5.3rc4",
+        "valer.core.spotlight==1.0.3rc4",
+        "valer.impress==1.2.4rc4",
+        "valer.jsonapi==1.2.3rc4",
+        "valer.panic==1.0.1rc4",
         "setuptools",
     ],
     extras_require={
